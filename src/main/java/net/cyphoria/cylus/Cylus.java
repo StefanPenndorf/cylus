70cb75cd04ca8581839066541ca30601fc23505f
Stefan <stefan@cyphoria.net>
2014-07-24 22:31:56 +0200

    Anwendung implementiert erste Schritte

---
+++ src/main/java/net/cyphoria/cylus/Cylus.java
@@ -0,0 +1,39 @@
+package net.cyphoria.cylus;/*
+ * Copyright (c) Stefan Penndorf 2014
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Stefan Penndoorf <stefan@cyphoria.net>
+ */
+@EnableAutoConfiguration
+@RestController
+public class Cylus {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cylus.class, args);
+    }
+
+}
