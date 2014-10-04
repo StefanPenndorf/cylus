55d5965e530ef91390d75bc047ba1e3e6fc9e18f
Stefan <stefan@cyphoria.net>
2014-10-04 21:12:18 +0200

    Von bootstrap.min auf bootstrap mit less umgestellt: Template Header fehlen noch (referenzieren noch boostrap.min

--- src/test/java/net/cyphoria/cylus/acceptancetest/seiten/NeuesKontoSeite.java
+++ src/test/java/net/cyphoria/cylus/acceptancetest/seiten/NeuesKontoSeite.java
@@ -39,7 +39,7 @@ public class NeuesKontoSeite extends FluentPage {
     @FindBy(css = "#kontoArt")
     FluentWebElement kontoArt;
 
-    @FindBy(css = "input[type=submit]")
+    @FindBy(css = "button[type=submit]")
     FluentWebElement kontoAnlegen;
 
 
