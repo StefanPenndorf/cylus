22558f6ab17202eaa369d14d66570443769a2a8a
Stefan <stefan@cyphoria.net>
2017-10-31 21:49:21 +0100

    Erster integrationstest für Konto-Suche (Autocomplete)

--- app3/src/test/java/net/cyphoria/cylus/app/controller/KontoControllerTest.java
+++ app3/src/test/java/net/cyphoria/cylus/app/controller/KontoControllerTest.java
@@ -4,21 +4,23 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @author Stefan Pennndorf
  */
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = KontoController.class)
+@WebMvcTest(KontoController.class)
 class KontoControllerTest {
 
     @Autowired
@@ -34,9 +36,32 @@ class KontoControllerTest {
     }
 
     @Test
-    void foo() throws Exception {
-        mvc.perform(get("/konto/search").accept(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(status().isOk());
+    void findetAlleKontenOhneSuchbegriff() throws Exception {
+        mvc.perform(get("/konto/search")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .accept(MediaType.APPLICATION_JSON_UTF8)
+                        .param("term", ""))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("[0].id", is("1230")))
+                .andExpect(jsonPath("[0].value", is("1230")))
+                .andExpect(jsonPath("[0].label", is("1230 - ABC")))
+                .andExpect(jsonPath("[1].id", is("1010")))
+                .andExpect(jsonPath("[1].value", is("1010")))
+                .andExpect(jsonPath("[1].label", is("1010 - Spesen")))
+        ;
+    }
+
+    @Test
+    void findetKontenNachKontonummerAnfangAlsSuchbegriff() throws Exception {
+        mvc.perform(get("/konto/search")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .param("term", "10"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("[0].id", is("1010")))
+                .andExpect(jsonPath("[0].value", is("1010")))
+                .andExpect(jsonPath("[0].label", is("1010 - Spesen")))
+        ;
     }
 
 }
