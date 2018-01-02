8feb03bfda5de12048e607fa3b0f7602d06ad7c1
Stefan <stefan@cyphoria.net>
2018-01-02 22:47:40 +0100

    Docker Swarm + Postgres
    - Wegen NEO4J OGM Issue 444 auf Postgres gewechselt (https://github.com/neo4j/neo4j-ogm/issues/444)
    - Docker Swarm mit Docker Secrets eingerichtet

--- create-cylus-prod.sh
+++ create-cylus-prod.sh
@@ -3,6 +3,7 @@
 docker-machine create --driver virtualbox --engine-insecure-registry stefan-desktop:5000 production
 docker-machine start production
 eval $(docker-machine env production)
+docker-machine ssh production "docker swarm init --advertise-addr `docker-machine ip production`"
 docker-machine ssh production -f -N -L 0.0.0.0:8000:localhost:8080
-docker-compose up -d
+docker stack deploy --prune -c docker-compose.yml cylus
 docker ps
