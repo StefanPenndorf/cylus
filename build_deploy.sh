107faed32df203f0e466725367a47b14491abe95
Stefan <stefan@cyphoria.net>
2018-02-04 17:11:36 +0100

    Debug-Infos in Build-Skript wieder deaktiviert, Tests aktiviert

--- build_deploy.sh
+++ build_deploy.sh
@@ -2,11 +2,11 @@
 
 eval $(docker-machine env -u)
 
-(cd ./app3/ && mvn install -DskipTests dockerfile:push)
+(cd ./app3/ && mvn install dockerfile:push)
 
 ./deploy-cylus-prod.sh
 
-eval $(docker-machine env production)
-
-docker service logs --since 1m --follow cylus_cylus
+#Debug
+#eval $(docker-machine env production)
+#docker service logs --since 1m --follow cylus_cylus
 
